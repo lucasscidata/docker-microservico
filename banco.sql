bd8b441f451dc5492e7ee9c49812751bba731da7
Lucas <lucas.scidata@gmail.com>
2024-11-21 09:10:37 -0300

    alteração da tabela e nos dados inseridos no index

--- banco.sql
+++ banco.sql
@@ -1,8 +1,9 @@
-CREATE TABLE dados (
-    AlunoID int,
-    Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
-    Cidade varchar(50),
-    Host varchar(50)
+CREATE TABLE Produtos (
+    idProduto INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(255) NOT NULL,
+    valor DECIMAL(10, 2) NOT NULL,
+    fornecedor VARCHAR(100),
+    dataFabricacao DATE,
+    dataValidade DATE,
+    Host VARCHAR(50)
 );
